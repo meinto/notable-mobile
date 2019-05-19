e8722054027cfad8e9e7afd64a0edf716154dfcb
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-05-19 19:26:31 +0200

    Setting up react-native-navigation for android

--- android/app/src/main/java/com/notablemobile/MainApplication.java
+++ android/app/src/main/java/com/notablemobile/MainApplication.java
@@ -8,38 +8,42 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
+import com.reactnativenavigation.react.ReactGateway;
+
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends NavigationApplication {
+    
+  @Override
+  protected ReactGateway createReactGateway() {
+      ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
+          @Override
+          protected String getJSMainModuleName() {
+              return "index";
+          }
+      };
+      return new ReactGateway(this, isDebug(), host);
+  }
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
+  @Override
+  public boolean isDebug() {
       return BuildConfig.DEBUG;
-    }
+  }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
+  protected List<ReactPackage> getPackages() {
+      // Add additional packages you require here
+      // No need to add RnnPackage and MainReactPackage
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          // eg. new VectorIconsPackage()
       );
-    }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
   }
 
   @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+  public List<ReactPackage> createAdditionalReactPackages() {
+      return getPackages();
   }
+  
 }
